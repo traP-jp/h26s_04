293fcc4014d173a8ef6be924b736f5d318604cfb
quaran <skrsoma37@gmail.com>
2026-06-27 15:15:12 +0900

    カードを球面に配置（リアクティブ前提） Fixes #8

--- src/views/ThreeDPage.vue
+++ src/views/ThreeDPage.vue
@@ -1,11 +1,61 @@
 <script setup lang="ts">
+import type { Message } from '@traptitech/traq'
+
+import { ref, watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
+
 import { TresCanvas } from '@tresjs/core'
 import { Vector3 } from 'three'
 
-import RotatingCube from '/@/components/3d/RotatingCube.vue'
+import MessageSphere from '/@/components/3d/MessageSphere.vue'
+import useChannelPath from '/@/composables/useChannelPath'
+import apis from '/@/lib/apis'
+import { useChannelTree } from '/@/store/domain/channelTree'
+import { useMessagesView } from '/@/store/domain/messagesView'
+import { useChannelsStore } from '/@/store/entities/channels'
+import { useMessagesStore } from '/@/store/entities/messages'
+import useInitialFetch from '/@/views/composables/useInitialFetch'
 
-const cameraPos = new Vector3(3, 3, 3)
+const cameraPos = new Vector3(0, 0, 90)
 const lightPos = new Vector3(5, 5, 5)
+
+const route = useRoute()
+const { channelPathStringToId } = useChannelPath()
+const { channelTree } = useChannelTree()
+const { fetchChannels } = useChannelsStore()
+const { extendMessagesMap } = useMessagesStore()
+const { renderMessageContent } = useMessagesView()
+
+const messages = ref<Message[]>([])
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+useInitialFetch(() => {})
+
+watchEffect(async () => {
+  const channelParam = route.params['channel'] as string
+  if (!channelParam) return
+
+  // track channelTree.children before any await so watchEffect re-runs when tree is populated
+  if (channelTree.value.children.length === 0) {
+    fetchChannels()
+    return
+  }
+
+  const channelId = channelPathStringToId(channelParam)
+  const res = await apis.getMessages(
+    channelId,
+    50,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    'desc'
+  )
+  const fetched = [...res.data].reverse()
+  extendMessagesMap(fetched)
+  messages.value = fetched
+  await Promise.all(fetched.map(msg => renderMessageContent(msg.id)))
+})
 </script>
 
 <template>
@@ -14,7 +64,7 @@ const lightPos = new Vector3(5, 5, 5)
       <TresPerspectiveCamera :position="cameraPos" :look-at="[0, 0, 0]" />
       <TresAmbientLight :intensity="1" />
       <TresDirectionalLight :position="lightPos" :intensity="1" />
-      <RotatingCube />
+      <MessageSphere :messages="messages" />
     </TresCanvas>
   </div>
 </template>
