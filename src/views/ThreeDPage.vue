37d031f2563277b0602ff8b664d0ae82b0e8bd02
quaran <skrsoma37@gmail.com>
2026-06-27 10:02:57 +0900

    サンプルを追加

---
+++ src/views/ThreeDPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import RotatingCube from '/@/components/3d/RotatingCube.vue'
+import { Vector3 } from 'three'
+const cameraPos = new Vector3(3, 3, 3)
+const lightPos = new Vector3(5, 5, 5)
+</script>
+
+<template>
+  <div style="width: 100vw; height: 100vh;">
+    <TresCanvas>
+      <TresPerspectiveCamera :position="cameraPos" :look-at="[0, 0, 0]" />
+      <TresAmbientLight :intensity="1" />
+      <TresDirectionalLight :position="lightPos" :intensity="1" />
+      <RotatingCube />
+    </TresCanvas>
+  </div>
+</template>
