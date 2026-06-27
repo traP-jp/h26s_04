8273d333eca3b51d3b4c0fee72570b13918cc4cc
gurukun41 <kouga1582@gmail.com>
2026-06-27 18:02:02 +0900

    数字の遷移の共通化

--- src/components/Main/MainView/ChannelView/ChannelSidebar/ChannelSidebarMessageCount.vue
+++ src/components/Main/MainView/ChannelView/ChannelSidebar/ChannelSidebarMessageCount.vue
@@ -9,27 +9,18 @@
         {{ messageCountText }}
       </span>
       <span v-else :class="$style.count">
-        <template v-for="(char, index) in messageCountChars" :key="index">
-          <span v-if="char === ','" :class="$style.separator">
-            {{ char }}
-          </span>
-          <span v-else :class="$style.digitSlot">
-            <transition :name="countTransitionName">
-              <span :key="`${index}-${char}`" :class="$style.digit">
-                {{ char }}
-              </span>
-            </transition>
-          </span>
-        </template>
+        <ChannelSidebarAnimatedNumber :value="totalMessageCount ?? 0" />
       </span>
     </div>
   </SidebarContentContainer>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref, watch } from 'vue'
+import { computed } from 'vue'
 
 import SidebarContentContainer from '/@/components/Main/MainView/PrimaryViewSidebar/SidebarContentContainer.vue'
+
+import ChannelSidebarAnimatedNumber from './ChannelSidebarAnimatedNumber.vue'
 
 const props = defineProps<{
   totalMessageCount?: number
@@ -37,17 +28,8 @@ const props = defineProps<{
   isFailed: boolean
 }>()
 
-const countDirection = ref<'up' | 'down'>('up')
-const isDigitTransitionEnabled = ref(false)
-
 const isMessageCountVisible = computed(
   () => !props.isFailed && props.totalMessageCount !== undefined
-)
-
-const countTransitionName = computed(() =>
-  isDigitTransitionEnabled.value
-    ? `count-digit-${countDirection.value}`
-    : undefined
 )
 
 const messageCountText = computed(() => {
@@ -58,26 +40,6 @@ const messageCountText = computed(() => {
 
   return props.totalMessageCount.toLocaleString()
 })
-
-const messageCountChars = computed(() =>
-  isMessageCountVisible.value && props.totalMessageCount !== undefined
-    ? props.totalMessageCount.toLocaleString().split('')
-    : []
-)
-
-watch(
-  () => props.totalMessageCount,
-  (newValue, oldValue) => {
-    isDigitTransitionEnabled.value = false
-
-    if (oldValue === undefined) return
-    if (newValue === undefined) return
-    if (newValue === oldValue) return
-
-    countDirection.value = newValue > oldValue ? 'up' : 'down'
-    isDigitTransitionEnabled.value = true
-  }
-)
 </script>
 
 <style lang="scss" module>
@@ -97,53 +59,6 @@ watch(
   &[data-is-muted] {
     @include color-ui-tertiary;
     font-weight: normal;
-  }
-}
-
-.digitSlot {
-  display: inline-grid;
-  grid-template: 'digit' 1.2em / 0.6em;
-  place-items: center;
-  height: 1.2em;
-  overflow: hidden;
-}
-
-.digit,
-.separator {
-  display: inline-block;
-}
-
-.digit {
-  grid-area: digit;
-}
-
-.separator {
-  min-width: 0.3em;
-}
-
-:global(.count-digit-up-enter-active),
-:global(.count-digit-up-leave-active),
-:global(.count-digit-down-enter-active),
-:global(.count-digit-down-leave-active) {
-  transition: transform 90ms ease-out;
-}
-
-:global(.count-digit-up-enter-from),
-:global(.count-digit-down-leave-to) {
-  transform: translateY(85%);
-}
-
-:global(.count-digit-up-leave-to),
-:global(.count-digit-down-enter-from) {
-  transform: translateY(-85%);
-}
-
-@media (prefers-reduced-motion: reduce) {
-  :global(.count-digit-up-enter-active),
-  :global(.count-digit-up-leave-active),
-  :global(.count-digit-down-enter-active),
-  :global(.count-digit-down-leave-active) {
-    transition: none;
   }
 }
 </style>
