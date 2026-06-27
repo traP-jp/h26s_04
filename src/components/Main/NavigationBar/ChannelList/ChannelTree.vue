b9795374716be8a0a05d9be7d9f587dafca5baf6
ramdos0207 <dev@ramdos.net>
2026-06-27 16:23:19 +0900

    add monaspace

--- src/components/Main/NavigationBar/ChannelList/ChannelTree.vue
+++ src/components/Main/NavigationBar/ChannelList/ChannelTree.vue
@@ -63,8 +63,11 @@ const toggleChildren = (channelId: ChannelId) => {
   scrollbar-width: none;
   -ms-overflow-style: none;
   font-family:
-    'JetBrains Mono', 'Cascadia Code', 'Fira Code', 'SFMono-Regular',
-    'Menlo', 'Consolas', monospace;
+    'Monaspace Neon', 'Monaspace Argon', 'Monaspace Xenon',
+    'Monaspace Krypton', 'Monaspace Radon', 'Monaspace', 'JetBrains Mono',
+    'Cascadia Code', 'Fira Code', 'SFMono-Regular', 'Menlo', 'Consolas',
+    monospace;
+  font-feature-settings: 'calt' 1;
   font-size: 0.92rem;
   letter-spacing: -0.02em;
 
